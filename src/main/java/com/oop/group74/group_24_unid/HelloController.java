c4ffe6f415263c89653d180aead5ff8edd4c17c5
Pratik <2220080@iub.edu.bd>
2025-07-27 09:16:11 +0600

    Initial commit

---
+++ src/main/java/com/oop/group74/group_24_unid/HelloController.java
@@ -0,0 +1,14 @@
+package com.oop.group74.group_24_unid;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.oop.group74.group_24_unid {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.oop.group74.group_24_unid to javafx.fxml;
+    exports com.oop.group74.group_24_unid;
+}
